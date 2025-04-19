4f55666e1c461424c32c6dc9f615cbfb0a46deab
Lynxprazo <issamwanga02@gmail.com>
2025-04-19 12:04:31 +0300

    more chages

--- src/Authentic/authentic.jsx
+++ src/Authentic/authentic.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import RegisterForm from "../component/forms/reg";
+import "./authentic.css";
 
 export default function Authentic() {
   return (
-    <div className='Authentic-maincontain'>
-      <div className="formcontainer-authentic">
-        
+    <div className="Authentic-maincontain">
+      <div className="maincontainer-authentic">
+        <div className="formcontainer-authentic">
+        <div className="left-container">
+          <RegisterForm/>
+        </div>
+        <div className="right"></div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/component/forms/reg.jsx
+++ src/component/forms/reg.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+// From Uiverse.io by Yaya12085
+import React from 'react';
 
-export default function Registration() {
+const RegisterForm = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form className="form">
+      <p className="title">Register </p>
+      <p className="message">Signup now and get full access to our app. </p>
+      <div className="flex">
+        <label>
+          <input required placeholder="" type="text" className="input" />
+          <span>Firstname</span>
+        </label>
+
+        <label>
+          <input required placeholder="" type="text" className="input" />
+          <span>Lastname</span>
+        </label>
+      </div>  
+            
+      <label>
+        <input required placeholder="" type="email" className="input" />
+        <span>Email</span>
+      </label> 
+        
+      <label>
+        <input required placeholder="" type="password" className="input" />
+        <span>Password</span>
+      </label>
+      <label>
+        <input required placeholder="" type="password" className="input" />
+        <span>Confirm password</span>
+      </label>
+      <button className="submit">Submit</button>
+      <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+    </form>
+  );
+};
+
+export default RegisterForm;
