537d9d69646bfd86ba206b340f9464b3ae1ac0e7
Lynxprazo <issamwanga02@gmail.com>
2025-04-19 11:38:56 +0300

    more chages

--- src/Authentic/authentic.jsx
+++ src/Authentic/authentic.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Authentic() {
   return (
     <div className='Authentic-maincontain'>
-
+      <div className="formcontainer-authentic">
+        
+      </div>
     </div>
   )
 }
